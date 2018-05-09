e3f51313b38a2f113046fd3107692e5a87a03708
Jake Kopulsky <jakekopulsky@gmail.com>
2018-05-08 23:45:46 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Jack\'s React Boilerplate';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
